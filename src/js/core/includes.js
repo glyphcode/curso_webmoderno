aa2a4b0557c543eb19244fd6ca86c00d77386131
Leo Bruno <leobruno.developer@gmail.com>
2019-08-30 11:52:17 -0400

    arquivo include.js alterado conforme instrucao do suporte da cod3r

--- src/js/core/includes.js
+++ src/js/core/includes.js
@@ -1,19 +1,21 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 function loadIncludes(parent) {
-    if(!parent) parent = 'body';
-    $(parent).find('[wm-include]').each(function(i, e) {
-        const URL = $(e).attr('wm-include');
-        $.ajax({
-            URL,
-            success(data) {
-                $(e).html(data)
-                $(e).removeAttr('wm-include')
+  if (!parent) parent = "body";
+  $(parent)
+    .find("[wm-include]")
+    .each(function(_i, e) {
+      const url = $(e).attr("wm-include");
+      $.ajax({
+        url,
+        success(data) {
+          $(e).html(data);
+          $(e).removeAttr("wm-include");
 
-                loadIncludes(e)
-            }
-        })
+          loadIncludes(e);
+        }
+      });
     });
 }
 
-loadIncludes()
+loadIncludes();
